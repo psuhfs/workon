0635c3e5cbea9db0a28c5faa45014459910155cc
Anmol <a.anmolgarg@gmail.com>
2024-10-21 22:40:04 -0400

    up

---
+++ data.js
@@ -0,0 +1,11 @@
+let data = [
+  {
+    accessCode: "12345",
+    employeeName: "John Doe",
+    email: "johndoe@example.com",
+    shift: "9:00 AM - 5:00 PM",
+    pointsReason: "Tardy",
+    comments: "Late due to traffic.",
+    timestamp: "2024-10-21 10:35:00",
+  },
+];
