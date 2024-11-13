9602c3c595a56343e333613f2fcc262c8cc45c9c
Sandipsinh Rathod <zotbysandip@gmail.com>
2024-11-13 11:35:05 -0500

    update access codes

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,27 @@
 const ACCESS_CODE = "ACCESS_CODE"
 
 // TODO: move it to a secure location
-const validAccessCodes = ["abg6200", "ckt5383", "mby5247", "ijr5083"]
+const validAccessCodes = [
+  "abg6200",
+  "ckt5383",
+  "mby5247",
+  "ijr5083",
+  "jva5829",
+  "rnb5344",
+  "smb8616",
+  "pgu5002",
+  "jnh5311",
+  "axk6243",
+  "rjk6127",
+  "szm6402",
+  "adm6017",
+  "man5890",
+  "hvp5321",
+  "vqp5208",
+  "abr6174",
+  "rps6236",
+  "hww5203",
+]
 
 function getCookie(name) {
   const cookies = document.cookie.split("; ")
