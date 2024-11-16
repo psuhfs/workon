d13187e5f5382509b76afddd4a94ee3171fbb252
Anmol <a.anmolgarg@gmail.com>
2024-11-15 19:36:48 -0500

    fix issues in report submisison handling

--- report.php
+++ report.php
@@ -6,8 +6,7 @@ $password = "Hfs@905870698";
 $dbname = "u426693394_hfs";
 
 // Email recipients
-$recipients = ["anmol@psu.edu", "scheduler@psu.edu"];
-// $recipients = ["anmol@psu.edu", "ckt5383@psu.edu"];
+$recipients = ["anmol@psu.edu", "scheduler@psu.edu", "ckt5383@psu.edu"];
 
 // Create a connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -25,41 +24,65 @@ $pastSevenDaysResult = $conn->query($pastSevenDaysQuery);
 $highPointsQuery = "SELECT * FROM employee_total_points WHERE totalPoints > 5";
 $highPointsResult = $conn->query($highPointsQuery);
 
-// Prepare the email content
-$emailBody = "Points Report:\n\n";
+// Prepare the HTML email content
+$emailBody = "<html><body>";
+$emailBody .= "<h2>HFS Student Employee Points Report</h2>";
 
 // Entries from Test_Points table
-$emailBody .= "Entries from Test_Points (Past 7 Days):\n";
+$emailBody .= "<h3>Entries from Past 7 Days</h3>";
 if ($pastSevenDaysResult->num_rows > 0) {
+    $emailBody .= "<table border='1' cellpadding='5' cellspacing='0'>";
+    $emailBody .= "<tr><th>Access Code</th><th>Employee Name</th><th>Points</th><th>Shift Date</th><th>Reason</th><th>Comments</th></tr>";
     while ($row = $pastSevenDaysResult->fetch_assoc()) {
-        $emailBody .= "Access Code: {$row['accessCode']}, Employee Name: {$row['employeeName']}, Points: {$row['points']}, Shift Date: {$row['shiftDate']}, Reason: {$row['reason']}, Comments: {$row['comments']}\n";
+        $emailBody .= "<tr>
+            <td>{$row['accessCode']}</td>
+            <td>{$row['employeeName']}</td>
+            <td>{$row['points']}</td>
+            <td>{$row['shiftDate']}</td>
+            <td>{$row['reason']}</td>
+            <td>{$row['comments']}</td>
+        </tr>";
     }
+    $emailBody .= "</table>";
 } else {
-    $emailBody .= "No entries found for the past 7 days.\n";
+    $emailBody .= "<p>No entries found for the past 7 days.</p>";
 }
 
-$emailBody .= "\n";
+$emailBody .= "<br>";
 
 // Employees with more than 5 points
-$emailBody .= "Employees with More Than 5 Points:\n";
+$emailBody .= "<h3>Employees with More Than 5 Points</h3>";
 if ($highPointsResult->num_rows > 0) {
+    $emailBody .= "<table border='1' cellpadding='5' cellspacing='0'>";
+    $emailBody .= "<tr><th>Employee ID</th><th>Employee Name</th><th>Total Points</th></tr>";
     while ($row = $highPointsResult->fetch_assoc()) {
-        $emailBody .= "Employee ID: {$row['employeeId']}, Employee Name: {$row['employeeName']}, Total Points: {$row['totalPoints']}\n";
+        $emailBody .= "<tr>
+            <td>{$row['employeeId']}</td>
+            <td>{$row['employeeName']}</td>
+            <td>{$row['totalPoints']}</td>
+        </tr>";
     }
+    $emailBody .= "</table>";
 } else {
-    $emailBody .= "No employees have more than 5 points.\n";
+    $emailBody .= "<p>No employees have more than 5 points.</p>";
 }
+
+$emailBody .= "</body></html>";
+
+// Output the report to browser
+echo $emailBody;
 
 // Email subject and headers
 $subject = "Weekly Points Report";
-$headers = "From: info@medilance.in";
+$headers = "From: info@medilance.in\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-// Send the email to all recipients
+// Send the email to all recipients and print success/failure messages
 foreach ($recipients as $recipient) {
     if (mail($recipient, $subject, $emailBody, $headers)) {
-        echo "Email sent successfully to $recipient.\n";
+        echo "Email sent successfully to $recipient.<br>";
     } else {
-        echo "Failed to send email to $recipient.\n";
+        echo "Failed to send email to $recipient. Error: " . error_get_last()['message'] . "<br>";
     }
 }
 
