a4ba7aa4945679352adf53f78bffa03fc2f23774
Sandipsinh Rathod <zotbysandip@gmail.com>
2025-01-31 11:23:57 -0500

    fix auth

--- js/auth.js
+++ js/auth.js
@@ -1,25 +1,91 @@
-// Function to validate access code and redirect if valid
-function validateAccessCode() {
-  console.log("Validating access code...")
-  const accessCode = document.getElementById("access-code").value
-  if (validAccessCodes.includes(accessCode)) {
-    console.log("Access code is valid")
-    setCookie(ACCESS_CODE, accessCode)
-    document.location.href = "/app" // Redirect to /app
-  } else {
-    console.log("Invalid access code") // Optional: show an error message
-  }
+// Global BASE_URL
+const BASE_URL = 'https://hfs.ssdd.dev';
+// const BASE_URL = 'http://localhost:3000';
+
+// Function to authenticate user
+async function authenticateUser() {
+    console.log("Authenticating user...");
+    const username = document.getElementById("access-code").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        const response = await fetch(`${BASE_URL}/auth/signin`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({username, password}),
+        });
+
+        if (response.ok) {
+            console.log("Authentication successful");
+            document.location.href = "/app"; // Redirect to /app
+        } else {
+
+            let body = await response.json();
+            displayError("Authentication failed: " + body.message);
+        }
+    } catch (error) {
+        console.error("Error during authentication:", error);
+        displayError("An error occurred during authentication. Please try again.");
+    }
+}
+
+// Function to display error message
+function displayError(message) {
+    console.log(message);
+    const errorElement = document.getElementById("error-message");
+    if (errorElement) {
+        errorElement.textContent = message;
+        errorElement.style.display = "block";
+    } else {
+        console.error("Error element not found in the DOM");
+    }
+}
+
+// Function to check if user is authenticated
+async function isAuthenticated() {
+    try {
+        const response = await fetch(`${BASE_URL}/auth/authenticated`, {
+            method: 'POST',
+            credentials: 'include', // This ensures cookies are sent with the request
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            return data.authenticated;
+        } else {
+            // Remove the token from cookies
+            document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+            // Re-initiate auth (redirect to login page or show login form)
+            document.location.href = "/"; // Assuming "/" is the login page
+            return false;
+        }
+    } catch (error) {
+        console.error("Error checking authentication:", error);
+        // In case of error, we assume the user is not authenticated
+        return false;
+    }
 }
 
 // Add event listeners
-document.addEventListener("DOMContentLoaded", function () {
-  const accessCodeInput = document.getElementById("access-code")
+document.addEventListener("DOMContentLoaded", async function () {
+    // Run authentication on Enter key press in password field
+    document.addEventListener("keypress", async function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            await authenticateUser();
+        }
+    });
 
-  // Run validation on Enter key press
-  accessCodeInput.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-      event.preventDefault() // Prevent form submission
-      validateAccessCode() // Manually validate the access code
+    // Check authentication status on page load
+    let authenticated = await isAuthenticated();
+    console.log("Authenticated:", authenticated);
+    if (authenticated) {
+        console.log("User is already authenticated");
+        document.location.href = "/app"; // Redirect to /app if already authenticated
     }
-  })
-})
+});
